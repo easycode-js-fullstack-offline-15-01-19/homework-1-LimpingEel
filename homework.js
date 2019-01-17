b135617c98b87e6be88e20bcb32f6ca3e54d2d6f
LimpingEel <nik.doroshenko.00@ukr.net>
2019-01-18 00:08:40 +0200

    added task#3

--- homework.js
+++ homework.js
@@ -47,32 +47,48 @@ value = Math.PI.toFixed(2);
 console.log(value)
 
 // #2  Используя Math, найти максимальное и минимальное числа из представленного ряда 15, 11, 16, 12, 51, 12, 13, 51
-value = Math.max(15, 11, 16, 12, 51, 12, 13, 51)
+value = Math.max(15, 11, 16, 12, 51, 12, 13, 51);
 console.log(value)
 
-value = Math.min(15, 11, 16, 12, 51, 12, 13, 51)
+value = Math.min(15, 11, 16, 12, 51, 12, 13, 51);
 console.log(value)
 
 // #3a Получить случайное число и округлить его до двух цифр после запятой
-value = Math.random().toFixed(2)
+value = Math.random().toFixed(2);
 console.log(value)
 
 // #3b Получить случайное целое число от 0 до X. X - любое произвольное число. 
-value = Math.ceil(Math.random() * 500)
+value = Math.ceil(Math.random() * 500);
 console.log(value)
 
 // #4 Проверить результат вычисления 0.6 + 0.7 - как привести к нормальному виду (1.3)?
-let numbers = 0.6 + 0.7
-value = numbers == 1.3
+let numbers = 0.6 + 0.7;
+value = numbers == 1.3;
 console.log(value)
 
-value = numbers.toFixed(1)
+value = numbers.toFixed(1);
 console.log(value)
 
 // #5 Получить число из строки ‘100$’
-let money = '100$'
-value = parseFloat(money)
+let money = '100$';
+value = parseFloat(money);
 console.log(value)
 
 
 
+// Объекты. Задачи.
+// #1 Создать объект с полем product, равным ‘iphone’
+let shop = {
+	product: 'iphone'
+};
+console.log(shop)
+
+// #2 Добавить в объект поле price, равное 1000 и поле currency, равное ‘dollar’
+shop.price = 1000;
+shop.currency = 'dollar';
+
+// #3 Добавить поле details, которое будет содержать объект с полями model и color
+shop.details = {
+	model: 'X',
+	color: 'Silver'
+};
